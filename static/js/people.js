907f946a80b0210b7dd97a1ffa52e9edc8aaf97f
Stanley Nguyen <hung.ngn.the@gmail.com>
2016-06-26 13:45:58 +0800

    befire modify program

--- static/js/people.js
+++ static/js/people.js
@@ -2,24 +2,24 @@ $(document).ready(function(){
 	// var width = $('#advisors').width();
 	// var height = $('#advisors').height();
 	$('#all').click(function(){
-		$('#advisors').show();
-		$('#mentors').show();
-		$('#team').show();
+		$('.advisors').show();
+		$('.mentors').show();
+		$('.team').show();
 	});
 	$('#advisors-trigger').click(function(){
-		$('#advisors').show();
-		$('#mentors').hide();
-		$('#team').hide();	
+		$('.advisors').show();
+		$('.mentors').hide();
+		$('.team').hide();	
 	})
 	$('#mentors-trigger').click(function(){
-		$('#advisors').hide();
-		$('#mentors').show();
-		$('#team').hide();
+		$('.advisors').hide();
+		$('.mentors').show();
+		$('.team').hide();
 	})
 	$('#team-trigger').click(function(){
-		$('#advisors').hide();
-		$('#team').show();
-		$('#mentors').hide();
+		$('.advisors').hide();
+		$('.team').show();
+		$('.mentors').hide();
 	})
 	$('.title a').click(function(){
 		$('.title a').removeClass('active');
