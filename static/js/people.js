01f2d528d21cd2a87fd45bb0343792bd20149399
Stanley Nguyen <hung.ngn.the@gmail.com>
2016-07-04 21:28:06 +0800

    populate pictures to people page

--- static/js/people.js
+++ static/js/people.js
@@ -3,21 +3,31 @@ $(document).ready(function(){
 		$('.advisors').show();
 		$('.mentors').show();
 		$('.team').show();
+		$('.management').show();
 	});
 	$('#advisors-trigger').click(function(){
 		$('.advisors').show();
 		$('.mentors').hide();
-		$('.team').hide();	
+		$('.team').hide();
+		$('.management').hide();
 	})
 	$('#mentors-trigger').click(function(){
 		$('.advisors').hide();
 		$('.mentors').show();
 		$('.team').hide();
+		$('.management').hide();
 	})
 	$('#team-trigger').click(function(){
 		$('.advisors').hide();
 		$('.team').show();
 		$('.mentors').hide();
+		$('.management').hide();
+	})
+	$('#management-trigger').click(function(){
+		$('.advisors').hide();
+		$('.team').hide();
+		$('.mentors').hide();
+		$('.management').show();
 	})
 	$('.title a').click(function(){
 		$('.title a').removeClass('active');
