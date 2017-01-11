ae14fc016370a9d5f679860521b3f17ed741b757
Stanley Nguyen <hung.ngn.the@gmail.com>
2017-01-11 11:56:50 +0800

    event page done

---
+++ static/js/events.js
@@ -0,0 +1,47 @@
+var eventsList = [];
+var eventsContainer = document.querySelector('#events .events');
+
+$(document).on('click', '#all', displayEvents.bind(null, eventsList));
+$(document).on('click', '#past', function() {
+  var pastEvents = eventsList.filter(function(e) {return e.status === 'completed'});
+
+  $('.title').children().removeClass('active');
+  $('#past').addClass('active');
+  displayEvents(pastEvents);
+});
+$(document).on('click', '#current', function() {
+  $('.title').children().removeClass('active');
+  $('#current').addClass('active');
+  var currentEvents = eventsList.filter(function(e) {return e.status === 'live'});
+  displayEvents(currentEvents);
+})
+
+$.ajax({
+  url: 'https://www.eventbriteapi.com/v3/users/me/owned_events/?token=F6G2U7K4IPMCFCZYTSUQ',
+  type: 'GET'
+}).done(function(res) {
+  eventsList = res.events;
+  displayEvents(eventsList);
+}).fail(function(e) {});
+
+function displayEvents(eventsList) {
+  eventsContainer.innerHTML = eventsList.map(function(e) {
+    var image = (e.logo) ? e.logo.url : '/img/logos/eventbrite.png';
+    var description = e.description.text || 'No Description';
+    var datetime = new Date(e.start.utc);
+    var fullDateTime = datetime.toLocaleString();
+    return '\
+      <div class="col-md-4">\
+        <div class="card">\
+          <img class="card-img-top" src="' + image + '" />\
+          <div class="card-block">\
+            <h4 class="card-title">' + e.name.text + '</h4>\
+            <p class="card-text text-muted">' + fullDateTime + '</p>\
+            <p class="card-text">' + description + '</p>\
+            <a href="' + e.url + '" target="_blank" class="btn btn-danger">RSVP</a>\
+          </div>\
+        </div>\
+      </div>\
+    ';
+  }).join('');
+}
