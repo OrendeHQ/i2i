5b1e240be94eaa1f6aadc86e084508cbb0b397d9
Stanley Nguyen <hung.ngn.the@gmail.com>
2017-01-28 12:29:10 +0800

    translucent overlay for navbar

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -17,3 +17,11 @@ $mailingModal.on('submit', 'form', function() {
     console.log(e);
   });
 });
+
+$(document).on('scroll', function() {
+  if (this.body.scrollTop > window.innerHeight) {
+    $('.navbar-overlay').css('opacity', '0');
+  } else {
+    $('.navbar-overlay').css('opacity', (window.innerHeight - this.body.scrollTop) / window.innerHeight);
+  }
+});
