3a8640aa448cde94d9aa5a035c7237539b9145b5
Stanley Nguyen <hung.ngn.the@gmail.com>
2017-04-28 23:14:46 +0800

    [Resolves] small iteration may2017

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,7 +1,9 @@
 $(document).on('scroll', function() {
    if (this.body.scrollTop > window.innerHeight) {
      $('.navbar-overlay').css('opacity', '0');
+     $('.brand').addClass('brand-reduced');
    } else {
      $('.navbar-overlay').css('opacity', (window.innerHeight - this.body.scrollTop) / window.innerHeight);
+     $('.brand').removeClass('brand-reduced');
    }
 });
--- static/js/people.js
+++ static/js/people.js
@@ -4,35 +4,35 @@ $(document).ready(function(){
 		$('.mentors').fadeIn();
 		$('.founding').fadeIn();
 		$('.management').fadeIn();
-		$('.project-wrapper').css('padding-right', '25px');
+		// $('.project-wrapper').css('padding-right', '25px');
 	});
 	$('#advisors-trigger').click(function(){
 		$('.mentors').fadeOut();
 		$('.founding').fadeOut();
 		$('.management').fadeOut();
 		$('.advisors').fadeIn();
-		$('.project-wrapper').css('padding-right', '180px');
+		// $('.project-wrapper').css('padding-right', '180px');
 	})
 	$('#mentors-trigger').click(function(){
 		$('.advisors').fadeOut();
 		$('.founding').fadeOut();
 		$('.management').fadeOut();
 		$('.mentors').fadeIn();
-		$('.project-wrapper').css('padding-right', '80px');
+		// $('.project-wrapper').css('padding-right', '80px');
 	})
 	$('#founding-trigger').click(function(){
 		$('.advisors').fadeOut();
 		$('.mentors').fadeOut();
 		$('.management').fadeOut();
 		$('.founding').fadeIn();
-		$('.project-wrapper').css('padding-right', '80px');
+		// $('.project-wrapper').css('padding-right', '80px');
 	})
 	$('#management-trigger').click(function(){
 		$('.advisors').fadeOut();
 		$('.founding').fadeOut();
 		$('.mentors').fadeOut();
 		$('.management').fadeIn();
-		$('.project-wrapper').css('padding-right', '180px');
+		// $('.project-wrapper').css('padding-right', '180px');
 	})
 	$('.title a').click(function(){
 		$('.title a').removeClass('active');
